c2470424009e87fa5cfa5062381e37dbf20b3f22
Bryan McCoy <bmccoy@turner-industries.com>
2020-03-06 09:08:19 -0600

    validation

--- src/TeamManagerDemo.Core/Entities/State.cs
+++ src/TeamManagerDemo.Core/Entities/State.cs
@@ -5,6 +5,8 @@ namespace TeamManagerDemo.Core.Entities
     public class State : Entity
     {
         [MaxLength(50)]
+        [Required]
+        [StringLength(50, ErrorMessage = "Name is to long")]
         public string Name { get; set; }
     }
 }
